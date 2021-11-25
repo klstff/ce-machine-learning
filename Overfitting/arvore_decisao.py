ec655fd44d4557b4f2896c8b6c22b8ddb4841ee2
Kelly Steffany <kellysteffanys@gmail.com>
2021-11-25 02:14:31 -0300

    Estimativa automática da qualidade de conteúdo

--- Overfitting/arvore_decisao.py
+++ Overfitting/arvore_decisao.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from typing import Tuple, List
+
 def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     """
         Retorna o modelo a ser usado.
@@ -21,6 +22,7 @@ def cria_modelo(X:pd.DataFrame ,y:pd.Series ,min_samples_split:float):
     #retone o modelo por meio do método fit
     return decision_tree.fit(X, y)
 
+
 def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd.DataFrame,pd.DataFrame]:
     """
         A partir do DataFrame df, faz a divisão entre treino e teste obedecendo a proporção val_proporcao_treino.
@@ -29,13 +31,12 @@ def divide_treino_teste(df:pd.DataFrame, val_proporcao_treino:float) -> Tuple[pd
         Retorna uma tupla com o treino e teste separados
     """
     #1. obtenha o treino usando o método sample do DataFrame
-    df_treino = None
+    df_treino = df.sample(frac=val_proporcao_treino, random_state=1)
 
     #2. Para obter o teste, selecione as instancias que estão em df e não estão em df_treino (use o método drop)
-    df_teste = None
+    df_teste = df.drop(df_treino.index)
 
     return df_treino,df_teste
-
 
 
 def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.DataFrame, y_teste:pd.Series, min_samples_split:float) -> Tuple[List[float],float]:
@@ -47,21 +48,24 @@ def faz_classificacao(x_treino:pd.DataFrame, y_treino:pd.Series, x_teste:pd.Data
              o resultado retornado pelo método predict do modelo.
             - A acuracia (proporção de exemplos classificados corretamente)
                 dicas:
-                * caso tenhamos dois vetores a e b, ao fazer a operção a==b, ele retornará
+                - caso tenhamos dois vetores a e b, ao fazer a operção a==b, ele retornará
                 um vetor em que o valor  de cada posição i será igual a verdadeiro caso a==b.
-                * np.sum soma os valores de um vetor (considerando True=1 e False=0)
+                - np.sum soma os valores de um vetor (considerando True=1 e False=0)
     """
     #cria o modelo (use a função previamente criada)
-    model_dtree = None
+    model_dtree = cria_modelo(x_treino, y_treino, min_samples_split)
 
     #realiza a predição (use o método predict do modelo)
-    y_predicted = None
+    y_predicted = model_dtree.predict(x_teste)
 
     #calcule a acurácia
-    acuracia = None
+    #acuracia = acertos/|y_teste|
+    lista_acertos = y_predicted==y_teste
+    acertos = np.sum(lista_acertos)
+    acuracia = acertos/len(y_teste)
 
+    return y_predicted, acuracia
 
-    return y_predicted,acuracia
 
 def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
     """
@@ -84,9 +88,9 @@ def plot_performance_min_samples(X_treino,y_treino,X_teste,y_teste):
 
     for min_samples in np.arange(0.001,0.7,0.01):
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no teste
-        y_predicted, ac_teste = None
+        y_predicted, ac_teste = faz_classificacao(X_treino, y_treino, X_teste, y_teste, min_samples)
         #complete a linha abaixo com a função e parametros corretos para calcular a acurácia no treino
-        y_predicted, ac_treino = None
+        y_predicted, ac_treino = faz_classificacao(X_treino, y_treino, X_treino, y_treino, min_samples)
 
         #adiciona a acuracia no treino, no teste e o parametro min_samples
         arr_ac_treino.append(ac_treino)
